fd10c224edeeacd9f4b58d7a4f78a74ec0a1aeed
Camila Goncalves <camiladenise.96@gmail.com>
2025-01-17 23:29:25 -0300

    create components

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h2>COUNTER COMPONENTE</h2>',
-})
-export class CounterComponent{
+  template: `
+  <h3>Counter: {{counter}}</h3>
 
+  <button (click)="increaseBy(1)">+1</button>
+  <button (click)="reset()">RESET</button>
+  <button (click)="decreaseBy(1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  counter: number = 10;
+
+  increaseBy(value: number): void{
+    this.counter += value;
+  }
+  reset():void{
+    this.counter = 10;
+  }
+  decreaseBy(value:number):void{
+    this.counter -= value;
+  }
 }
