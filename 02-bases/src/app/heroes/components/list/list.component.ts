765ad9ef2903f5501a74d6a18171644cdfdddad2
Camila Goncalves <camiladenise.96@gmail.com>
2025-01-25 00:57:50 -0300

    dbz module

--- 02-bases/src/app/heroes/components/list/list.component.ts
+++ 02-bases/src/app/heroes/components/list/list.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
+
 export class ListComponent {
 
   public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
